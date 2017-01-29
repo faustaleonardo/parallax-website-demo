26b7ebabefb0a4b460485bc7eb3501c9e90e7032
faustaleonardo <faustaleonardo11@gmail.com>
2017-01-29 12:31:57 +0700

    landing elements

--- js/script.js
+++ js/script.js
@@ -9,16 +9,29 @@ $(document).ready(function(){
 		$('.logo').css({
 			'transform': 'translate(0px, ' + wScroll/2 + '%)'
 		});
-
 		$('.back-bird').css({
 			'transform': 'translate(0px, ' + wScroll/4 + '%)'
 		});
-
 		// - to make it going up 
 		$('.fore-bird').css({
 			'transform': 'translate(0px, -' + wScroll/40 + '%)'
 		});
 
+		// to decide when to display the animation
+		if (wScroll > ($('.clothes-pics').offset().top - $(window).height() / 1.2)){
+
+			// i starts with 0
+			$('.clothes-pics figure').each(function(i){
+				setTimeout(function(){
+					// eq means index
+					$('.clothes-pics figure').eq(i).addClass('is-showing');
+				}, 150 * (i+1));
+			});
+
+		}
+
 	});
+
+
 
 });
