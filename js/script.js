4e8cee54c5a3874272563bc6ea381b3207b4c1ce
faustaleonardo <faustaleonardo11@gmail.com>
2017-01-29 21:04:03 +0700

    fix bugs

--- js/script.js
+++ js/script.js
@@ -13,18 +13,22 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		var wScroll = $(this).scrollTop();
 
-		// translate(speedToGoHorizontal, speedToGoVertical)
-		// + to make them going down
-		$('.logo').css({
-			'transform': 'translate(0px, ' + wScroll/2 + '%)'
-		});
-		$('.back-bird').css({
-			'transform': 'translate(0px, ' + wScroll/4 + '%)'
-		});
-		// - to make it going up 
-		$('.fore-bird').css({
-			'transform': 'translate(0px, -' + wScroll/40 + '%)'
-		});
+		var pContainerHeight = $('.bird-box').height();
+		// make sure only do parallax when the scroll is within the height of bird-box
+		if(wScroll <= pContainerHeight){
+			// translate(speedToGoHorizontal, speedToGoVertical)
+			// + to make them going down
+			$('.logo').css({
+				'transform': 'translate(0px, ' + wScroll/2 + '%)'
+			});
+			$('.back-bird').css({
+				'transform': 'translate(0px, ' + wScroll/4 + '%)'
+			});
+			// - to make it going up 
+			$('.fore-bird').css({
+				'transform': 'translate(0px, -' + wScroll/40 + '%)'
+			});
+		}
 
 		// to decide when to display the animation
 		if (wScroll > ($('.clothes-pics').offset().top - $(window).height() / 1.2)){
@@ -44,7 +48,8 @@ $(document).ready(function(){
 
 			// opacity becoming higher when user scrolls more down
 			// to get value between 0-1
-			// 400 and /5 are used to make the element range between 0.9-0.1 (two digits)
+			// /5 are used to make the element range between 0.9-0.1 (two digits)
+			// when the range from the top of window to the element reach 400
 			var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
 
 			$('.window-tint').css({'opacity' : opacity});
